fbc095ab8ab0224eae462a7359b85d65bf3909a6
eduforci <ouattaramassa639@gmail.com>
2026-09-18 07:38:03 +0000

    Update table headers for student notes

--- app/directeur-etudes/classes/[classeId]/liste/page.tsx
+++ app/directeur-etudes/classes/[classeId]/liste/page.tsx
@@ -143,12 +143,12 @@ export default function ListeClassePage() {
             <th className="border p-1 w-10">N°</th>
             <th className="border p-1 text-left">Matricule</th>
             <th className="border p-1 text-left">Nom et Prénoms</th>
-            <th className="border p-1 w-8"></th>
-            <th className="border p-1 w-8"></th>
-            <th className="border p-1 w-8"></th>
-            <th className="border p-1 w-8"></th>
-            <th className="border p-1 w-8"></th>
-            <th className="border p-1 w-8"></th>
+            <th className="border p-1 w-14">Note 1</th>
+            <th className="border p-1 w-14">Note 2</th>
+            <th className="border p-1 w-14">Note 3</th>
+            <th className="border p-1 w-14">Note 4</th>
+            <th className="border p-1 w-14">Moy.</th>
+            <th className="border p-1 w-14">Rang</th>
           </tr>
         </thead>
         <tbody>
@@ -183,4 +183,3 @@ export default function ListeClassePage() {
     </div>
   );
 }
-  
